d9945caa3c6c8c35c2ea42bf07cafd186c8cb342
CK0629 <kuruta.dev00@gmail.com>
2021-08-22 20:43:11 +0900

    更新

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,11 @@ import Head from "next/head";
 
 export default function Layout({children, title = "Default title" }) {
 	return (
-		<div className="flex justify-center items-center flex-col h-screen md:h-screen text-gray-900 font-mono bg-gray-100">
+		<div className="flex justify-center items-center flex-col h-screen md:h-screen text-gray-900 font-mono bg-gray-100 inset-0">
 			<Head>
 				<title>{title}</title>
 			</Head>
-			<main className="flex justify-center items-center w-full flex-col">
+			<main className="flex flex-1 justify-center items-center w-full flex-col">
         		{children}
     		</main>
 		</div>
