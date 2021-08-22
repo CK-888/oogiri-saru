3638a1a532ad3d950681e302ef3f3dc88b18cf41
CK0629 <kuruta.dev00@gmail.com>
2021-08-22 20:25:15 +0900

    更新

--- components/Theme.js
+++ components/Theme.js
@@ -1,7 +1,7 @@
 export default function Theme() {
 	return (
 		<div className=" h-24 md:h-72 text-gray-900 bg-yellow-400 text-center align-text-bottom shadow-md m-8 rounded-xl">
-			<span>text</span>
+			text
 		</div>
 	);
 }
