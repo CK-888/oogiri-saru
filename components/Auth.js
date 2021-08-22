222b8be0b3f8143428e11af348dec7d23976c099
CK0629 <kuruta.dev00@gmail.com>
2021-08-22 15:25:31 +0900

    画像のHeightを更新

--- components/Auth.js
+++ components/Auth.js
@@ -63,54 +63,56 @@ export default function Auth() {
     }
   };
 
-	return <div className="max-w-md md:w-full space-y-8">
-    <div>
-      <img className="mx-auto h-24 w-auto" src="/ape_logo_size128.png" alt="ape" />
-      <h2 className="mt-6 text-center text-3xl font-sans font-extrabold text-gray-900">
-        大喜利のサイト        
-      </h2>
-    </div>
-    <form className="mt-8 space-y-6" onSubmit={authUser}>
-      <input type="hidden" name="remember" value="true" />
-      <div className="rounded-md shadow-sm -space-y-px">
-        <div>
-          <input 
-            name="username"
-            type="text"
-            autoComplete="username"
-            required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-            />
-        </div>
-        <div >
-          <input 
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-
+	return (
+    <div className="max-w-md md:w-full space-y-8">
       <div>
-        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-            <svg className="h-5 w-5 text-gray-900 group-hover:text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-            </svg>
-          </span>
-          Login
-        </button>
+        <img className="mx-auto w-auto" src="/ape_logo_size128.png" alt="ape" />
+        <h2 className="mt-6 text-center text-3xl font-sans font-extrabold text-gray-900">
+          大喜利のサイト
+        </h2>
       </div>
-    </form>
-  </div>
+      <form className="mt-8 space-y-6" onSubmit={authUser}>
+        <input type="hidden" name="remember" value="true" />
+        <div className="rounded-md shadow-sm -space-y-px">
+          <div>
+            <input 
+              name="username"
+              type="text"
+              autoComplete="username"
+              required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              />
+          </div>
+          <div >
+            <input 
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg className="h-5 w-5 text-gray-900 group-hover:text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+              </svg>
+            </span>
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
