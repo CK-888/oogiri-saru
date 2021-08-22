c574393a5675d712d1a8ac8ffa4cc2c35d554186
CK0629 <kuruta.dev00@gmail.com>
2021-08-22 15:26:20 +0900

    メインページ更新

---
+++ components/AnswerList.js
@@ -0,0 +1,19 @@
+
+export default function AnswerList() {
+	return (
+		<div className="h-auto w-auto m-8">
+			{/* 回答本文 */}
+			<div className="bg-red-100">
+				aaa
+			</div>
+			{/* いいね */}
+			<div className="bg-red-200">
+				☆
+			</div>
+			{/* コメント */}
+			<div className="bg-red-300">
+				○
+			</div>
+		</div>
+	);
+}
--- pages/oogiri-main-page.js
+++ pages/oogiri-main-page.js
@@ -1,16 +1,14 @@
 import Layout from "../components/Layout";
-
+import AnswerList from "../components/AnswerList";
+import Theme from "../components/Theme";
 
 export default function OogiriMainPage() {
 	return (
 		<Layout title="oogiri-main page">
 			<div className="container">
-				<div className="bg-yellow-500 m-8 h-32 md:h-full text-center">
-					こんなクリスマスは嫌だ。どんなクリスマス？
-				</div>
-			</div>
-			<div className="container">
-				answerList
+				<Theme />
+				<AnswerList />
+
 			</div>
 		</Layout>
 	);
